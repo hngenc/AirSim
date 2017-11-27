ce7021064fc91e11fcdd068649211f356ad7c5b2
Hasan Genc <hngenc@utexas.edu>
2017-11-26 19:42:57 -0600

    Added rudimentary automation capabilities to AirSim, including the ability to start a packaged game, to change maps, to restart levels, and to exit running games

--- Unreal/Plugins/AirSim/Source/Multirotor/SimModeWorldMultiRotor.cpp
+++ Unreal/Plugins/AirSim/Source/Multirotor/SimModeWorldMultiRotor.cpp
@@ -10,198 +10,290 @@
 
 ASimModeWorldMultiRotor::ASimModeWorldMultiRotor()
 {
-    static ConstructorHelpers::FClassFinder<APIPCamera> external_camera_class(TEXT("Blueprint'/AirSim/Blueprints/BP_PIPCamera'"));
-    external_camera_class_ = external_camera_class.Succeeded() ? external_camera_class.Class : nullptr;
-    static ConstructorHelpers::FClassFinder<ACameraDirector> camera_director_class(TEXT("Blueprint'/AirSim/Blueprints/BP_CameraDirector'"));
-    camera_director_class_ = camera_director_class.Succeeded() ? camera_director_class.Class : nullptr;
-    static ConstructorHelpers::FClassFinder<TMultiRotorPawn> vehicle_pawn_class(TEXT("Blueprint'/AirSim/Blueprints/BP_FlyingPawn'"));
-    vehicle_pawn_class_ = vehicle_pawn_class.Succeeded() ? vehicle_pawn_class.Class : nullptr;
+	static ConstructorHelpers::FClassFinder<APIPCamera> external_camera_class(TEXT("Blueprint'/AirSim/Blueprints/BP_PIPCamera'"));
+	external_camera_class_ = external_camera_class.Succeeded() ? external_camera_class.Class : nullptr;
+	static ConstructorHelpers::FClassFinder<ACameraDirector> camera_director_class(TEXT("Blueprint'/AirSim/Blueprints/BP_CameraDirector'"));
+	camera_director_class_ = camera_director_class.Succeeded() ? camera_director_class.Class : nullptr;
+	static ConstructorHelpers::FClassFinder<TMultiRotorPawn> vehicle_pawn_class(TEXT("Blueprint'/AirSim/Blueprints/BP_FlyingPawn'"));
+	vehicle_pawn_class_ = vehicle_pawn_class.Succeeded() ? vehicle_pawn_class.Class : nullptr;
 }
 
 void ASimModeWorldMultiRotor::BeginPlay()
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
 
-    if (fpv_vehicle_connector_ != nullptr) {
-        //create its control server
-        try {
-            fpv_vehicle_connector_->startApiServer();
-        }
-        catch (std::exception& ex) {
-            UAirBlueprintLib::LogMessageString("Cannot start RpcLib Server", ex.what(), LogDebugLevel::Failure);
-        }
-    }
+	if (fpv_vehicle_connector_ != nullptr) {
+		//create its control server
+		try {
+			fpv_vehicle_connector_->startApiServer();
+		}
+		catch (std::exception& ex) {
+			UAirBlueprintLib::LogMessageString("Cannot start RpcLib Server", ex.what(), LogDebugLevel::Failure);
+		}
+	}
 
 }
 
 void ASimModeWorldMultiRotor::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-    //stop physics thread before we dismental
-    stopAsyncUpdator();
+	//stop physics thread before we dismental
+	stopAsyncUpdator();
 
-    if (fpv_vehicle_connector_ != nullptr) {
-        fpv_vehicle_connector_->stopApiServer();
-        fpv_vehicle_pawn_wrapper_ = nullptr;
-    }
+	if (fpv_vehicle_connector_ != nullptr) {
+		fpv_vehicle_connector_->stopApiServer();
+		fpv_vehicle_pawn_wrapper_ = nullptr;
+	}
 
-    //for (AActor* actor : spawned_actors_) {
-    //    actor->Destroy();
-    //}
-    spawned_actors_.Empty();
-    if (CameraDirector != nullptr) {
-        fpv_vehicle_connector_ = nullptr;
-        CameraDirector = nullptr;
-    }
+	//for (AActor* actor : spawned_actors_) {
+	//    actor->Destroy();
+	//}
+	spawned_actors_.Empty();
+	if (CameraDirector != nullptr) {
+		fpv_vehicle_connector_ = nullptr;
+		CameraDirector = nullptr;
+	}
 
-    Super::EndPlay(EndPlayReason);
+	Super::EndPlay(EndPlayReason);
 }
 
 VehiclePawnWrapper* ASimModeWorldMultiRotor::getFpvVehiclePawnWrapper()
 {
-    return fpv_vehicle_pawn_wrapper_;
+	return fpv_vehicle_pawn_wrapper_;
 }
 
 void ASimModeWorldMultiRotor::setupVehiclesAndCamera(std::vector<VehiclePtr>& vehicles)
 {
-    //get player controller
-    APlayerController* player_controller = this->GetWorld()->GetFirstPlayerController();
-    FTransform actor_transform = player_controller->GetActorTransform();
-    //put camera little bit above vehicle
-    FTransform camera_transform(actor_transform.GetLocation() + FVector(-300, 0, 200));
+	//get player controller
+	APlayerController* player_controller = this->GetWorld()->GetFirstPlayerController();
+	FTransform actor_transform = player_controller->GetActorTransform();
+	//put camera little bit above vehicle
+	FTransform camera_transform(actor_transform.GetLocation() + FVector(-300, 0, 200));
 
-    //we will either find external camera if it already exist in evironment or create one
-    APIPCamera* external_camera;
+	//we will either find external camera if it already exist in evironment or create one
+	APIPCamera* external_camera;
 
-    //find all BP camera directors in the environment
-    {
-        TArray<AActor*> camera_dirs;
-        UAirBlueprintLib::FindAllActor<ACameraDirector>(this, camera_dirs);
-        if (camera_dirs.Num() == 0) {
-            //create director
-            FActorSpawnParameters camera_spawn_params;
-            camera_spawn_params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
-            CameraDirector = this->GetWorld()->SpawnActor<ACameraDirector>(camera_director_class_, camera_transform, camera_spawn_params);
-            CameraDirector->setFollowDistance(225);
-            CameraDirector->setCameraRotationLagEnabled(false);
-            CameraDirector->setFpvCameraIndex(0);
-            CameraDirector->enableFlyWithMeMode();
-            spawned_actors_.Add(CameraDirector);
+	//find all BP camera directors in the environment
+	{
+		TArray<AActor*> camera_dirs;
+		UAirBlueprintLib::FindAllActor<ACameraDirector>(this, camera_dirs);
+		if (camera_dirs.Num() == 0) {
+			//create director
+			FActorSpawnParameters camera_spawn_params;
+			camera_spawn_params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+			CameraDirector = this->GetWorld()->SpawnActor<ACameraDirector>(camera_director_class_, camera_transform, camera_spawn_params);
+			CameraDirector->setFollowDistance(225);
+			CameraDirector->setCameraRotationLagEnabled(false);
+			CameraDirector->setFpvCameraIndex(0);
+			CameraDirector->enableFlyWithMeMode();
+			spawned_actors_.Add(CameraDirector);
 
-            //create external camera required for the director
-            external_camera = this->GetWorld()->SpawnActor<APIPCamera>(external_camera_class_, camera_transform, camera_spawn_params);
-            spawned_actors_.Add(external_camera);
-        }
-        else {
-            CameraDirector = static_cast<ACameraDirector*>(camera_dirs[0]);
-            external_camera = CameraDirector->getExternalCamera();
-        }
-    }
+			//create external camera required for the director
+			external_camera = this->GetWorld()->SpawnActor<APIPCamera>(external_camera_class_, camera_transform, camera_spawn_params);
+			spawned_actors_.Add(external_camera);
+		}
+		else {
+			CameraDirector = static_cast<ACameraDirector*>(camera_dirs[0]);
+			external_camera = CameraDirector->getExternalCamera();
+		}
+	}
 
-    //find all vehicle pawns
-    {
-        TArray<AActor*> pawns;
-        UAirBlueprintLib::FindAllActor<TMultiRotorPawn>(this, pawns);
+	//find all vehicle pawns
+	{
+		TArray<AActor*> pawns;
+		UAirBlueprintLib::FindAllActor<TMultiRotorPawn>(this, pawns);
 
-        //if no vehicle pawns exists in environment
-        if (pawns.Num() == 0) {
-            //create vehicle pawn
-            FActorSpawnParameters pawn_spawn_params;
-            pawn_spawn_params.SpawnCollisionHandlingOverride =
-                ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
-            TMultiRotorPawn* spawned_pawn = this->GetWorld()->SpawnActor<TMultiRotorPawn>(
-                vehicle_pawn_class_, actor_transform, pawn_spawn_params);
+		//if no vehicle pawns exists in environment
+		if (pawns.Num() == 0) {
+			//create vehicle pawn
+			FActorSpawnParameters pawn_spawn_params;
+			pawn_spawn_params.SpawnCollisionHandlingOverride =
+				ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+			TMultiRotorPawn* spawned_pawn = this->GetWorld()->SpawnActor<TMultiRotorPawn>(
+				vehicle_pawn_class_, actor_transform, pawn_spawn_params);
 
-            spawned_actors_.Add(spawned_pawn);
-            pawns.Add(spawned_pawn);
-        }
+			spawned_actors_.Add(spawned_pawn);
+			pawns.Add(spawned_pawn);
+		}
 
-        //set up vehicle pawns
-        for (AActor* pawn : pawns)
-        {
-            //initialize each vehicle pawn we found
-            TMultiRotorPawn* vehicle_pawn = static_cast<TMultiRotorPawn*>(pawn);
-            vehicle_pawn->initializeForBeginPlay();
+		//set up vehicle pawns
+		for (AActor* pawn : pawns)
+		{
+			//initialize each vehicle pawn we found
+			TMultiRotorPawn* vehicle_pawn = static_cast<TMultiRotorPawn*>(pawn);
+			vehicle_pawn->initializeForBeginPlay();
 
-            //chose first pawn as FPV if none is designated as FPV
-            VehiclePawnWrapper* wrapper = vehicle_pawn->getVehiclePawnWrapper();
-            if (enable_collision_passthrough)
-                wrapper->config.enable_passthrough_on_collisions = true;
-            if (wrapper->config.is_fpv_vehicle || fpv_vehicle_pawn_wrapper_ == nullptr)
-                fpv_vehicle_pawn_wrapper_ = wrapper;
+			//chose first pawn as FPV if none is designated as FPV
+			VehiclePawnWrapper* wrapper = vehicle_pawn->getVehiclePawnWrapper();
+			if (enable_collision_passthrough)
+				wrapper->config.enable_passthrough_on_collisions = true;
+			if (wrapper->config.is_fpv_vehicle || fpv_vehicle_pawn_wrapper_ == nullptr)
+				fpv_vehicle_pawn_wrapper_ = wrapper;
 
-            //now create the connector for each pawn
-            VehiclePtr vehicle = createVehicle(wrapper);
-            if (vehicle != nullptr) {
-                vehicles.push_back(vehicle);
+			//now create the connector for each pawn
+			VehiclePtr vehicle = createVehicle(wrapper);
+			if (vehicle != nullptr) {
+				vehicles.push_back(vehicle);
 
-                if (fpv_vehicle_pawn_wrapper_ == wrapper)
-                    fpv_vehicle_connector_ = vehicle;
-            }
-            //else we don't have vehicle for this pawn
-        }
-    }
+				if (fpv_vehicle_pawn_wrapper_ == wrapper)
+					fpv_vehicle_connector_ = vehicle;
+			}
+			//else we don't have vehicle for this pawn
+		}
+	}
 
-    CameraDirector->initializeForBeginPlay(getInitialViewMode(), fpv_vehicle_pawn_wrapper_, external_camera);
+	CameraDirector->initializeForBeginPlay(getInitialViewMode(), fpv_vehicle_pawn_wrapper_, external_camera);
+}
+
+// Hasan's additions
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine.h"
+
+// Hasan's additions
+static bool file_exists(const char * name) {
+	FILE * file = fopen(name, "r");
+	if (file) {
+		fclose(file);
+		return true;
+	}
+	return false;
+}
+
+// Hasan's additions
+static std::string file_contents(const char * name) {
+	std::string contents;
+
+	FILE * file = fopen(name, "r");
+	if (file) {
+		std::fseek(file, 0, SEEK_END);
+		contents.resize(std::ftell(file));
+		std::rewind(file);
+		std::fread(&contents[0], 1, contents.size(), file);
+
+		fclose(file);
+	}
+
+	return contents;
+}
+
+// Hasan's additions
+static bool time_to_restart() {
+	const std::string filename = "restart";
+	static std::string app_data_path = common_utils::FileSystem::getAppDataFolder();
+	static std::string path = common_utils::FileSystem::combine(app_data_path, filename);
+
+	if (file_exists(path.c_str())) {
+		remove(path.c_str());
+		return true;
+	}
+	return false;
+}
+
+// Hasan's additions
+static bool time_to_exit() {
+	const std::string filename = "exit";
+	static std::string app_data_path = common_utils::FileSystem::getAppDataFolder();
+	static std::string path = common_utils::FileSystem::combine(app_data_path, filename);
+
+	if (file_exists(path.c_str())) {
+		remove(path.c_str());
+		return true;
+	}
+	return false;
+}
+
+static std::string change_level_to() {
+	const std::string filename = "change_level";
+	static std::string app_data_path = common_utils::FileSystem::getAppDataFolder();
+	static std::string path = common_utils::FileSystem::combine(app_data_path, filename);
+
+	std::string new_level = file_contents(path.c_str());
+
+	if (!new_level.empty()) {
+		remove(path.c_str());
+	}
+	return new_level;
+}
+
+static void ExecScriptingCommands(ASimModeWorldMultiRotor * simModeWorldMultiRotor) {
+	UWorld * world = simModeWorldMultiRotor->GetWorld();
+
+	std::string new_level = change_level_to();
+	if (!new_level.empty()) {
+		UGameplayStatics::OpenLevel(simModeWorldMultiRotor, new_level.c_str(), false);
+	}
+
+	if (time_to_restart()) {
+		UGameplayStatics::OpenLevel(simModeWorldMultiRotor, FName(*world->GetName()), false);
+	}
+
+	if (time_to_exit()) {
+		APlayerController* PController = UGameplayStatics::GetPlayerController(world, 0);
+		PController->ConsoleCommand(TEXT("exit"));
+	}
 }
 
 void ASimModeWorldMultiRotor::Tick(float DeltaSeconds)
 {
-    Super::Tick(DeltaSeconds);
+	Super::Tick(DeltaSeconds);
 
-    getFpvVehiclePawnWrapper()->setLogLine(getLogString());
+	getFpvVehiclePawnWrapper()->setLogLine(getLogString());
+
+	// Hasan's additions
+	ExecScriptingCommands(this);
 }
 
 std::string ASimModeWorldMultiRotor::getLogString()
 {
-    const msr::airlib::Kinematics::State* kinematics = getFpvVehiclePawnWrapper()->getKinematics();
-    uint64_t timestamp_millis = static_cast<uint64_t>(msr::airlib::ClockFactory::get()->nowNanos() / 1.0E6);
+	const msr::airlib::Kinematics::State* kinematics = getFpvVehiclePawnWrapper()->getKinematics();
+	uint64_t timestamp_millis = static_cast<uint64_t>(msr::airlib::ClockFactory::get()->nowNanos() / 1.0E6);
 
-    //TODO: because this bug we are using alternative code with stringstream
-    //https://answers.unrealengine.com/questions/664905/unreal-crashes-on-two-lines-of-extremely-simple-st.html
+	//TODO: because this bug we are using alternative code with stringstream
+	//https://answers.unrealengine.com/questions/664905/unreal-crashes-on-two-lines-of-extremely-simple-st.html
 
-    std::string line;
-    line.append(std::to_string(timestamp_millis)).append("\t")
-        .append(std::to_string(kinematics->pose.position.x())).append("\t")
-        .append(std::to_string(kinematics->pose.position.y())).append("\t")
-        .append(std::to_string(kinematics->pose.position.z())).append("\t")
-        .append(std::to_string(kinematics->pose.orientation.w())).append("\t")
-        .append(std::to_string(kinematics->pose.orientation.x())).append("\t")
-        .append(std::to_string(kinematics->pose.orientation.y())).append("\t")
-        .append(std::to_string(kinematics->pose.orientation.z())).append("\t");
+	std::string line;
+	line.append(std::to_string(timestamp_millis)).append("\t")
+		.append(std::to_string(kinematics->pose.position.x())).append("\t")
+		.append(std::to_string(kinematics->pose.position.y())).append("\t")
+		.append(std::to_string(kinematics->pose.position.z())).append("\t")
+		.append(std::to_string(kinematics->pose.orientation.w())).append("\t")
+		.append(std::to_string(kinematics->pose.orientation.x())).append("\t")
+		.append(std::to_string(kinematics->pose.orientation.y())).append("\t")
+		.append(std::to_string(kinematics->pose.orientation.z())).append("\t");
 
-    return line;
+	return line;
 
-    //std::stringstream ss;
-    //ss << timestamp_millis << "\t";
-    //ss << kinematics.pose.position.x() << "\t" << kinematics.pose.position.y() << "\t" << kinematics.pose.position.z() << "\t";
-    //ss << kinematics.pose.orientation.w() << "\t" << kinematics.pose.orientation.x() << "\t" << kinematics.pose.orientation.y() << "\t" << kinematics.pose.orientation.z() << "\t";
-    //ss << "\n";
-    //return ss.str();
+	//std::stringstream ss;
+	//ss << timestamp_millis << "\t";
+	//ss << kinematics.pose.position.x() << "\t" << kinematics.pose.position.y() << "\t" << kinematics.pose.position.z() << "\t";
+	//ss << kinematics.pose.orientation.w() << "\t" << kinematics.pose.orientation.x() << "\t" << kinematics.pose.orientation.y() << "\t" << kinematics.pose.orientation.z() << "\t";
+	//ss << "\n";
+	//return ss.str();
 }
 
 void ASimModeWorldMultiRotor::createVehicles(std::vector<VehiclePtr>& vehicles)
 {
-    //find vehicles and cameras available in environment
-    //if none available then we will create one
-    setupVehiclesAndCamera(vehicles);
+	//find vehicles and cameras available in environment
+	//if none available then we will create one
+	setupVehiclesAndCamera(vehicles);
 }
 
 ASimModeWorldBase::VehiclePtr ASimModeWorldMultiRotor::createVehicle(VehiclePawnWrapper* wrapper)
 {
-    auto vehicle_params = MultiRotorParamsFactory::createConfig(
-        wrapper->config.vehicle_config_name == "" ? default_vehicle_config
-        : std::string(TCHAR_TO_UTF8(*wrapper->config.vehicle_config_name)));
+	auto vehicle_params = MultiRotorParamsFactory::createConfig(
+		wrapper->config.vehicle_config_name == "" ? default_vehicle_config
+		: std::string(TCHAR_TO_UTF8(*wrapper->config.vehicle_config_name)));
 
-    vehicle_params_.push_back(std::move(vehicle_params));
+	vehicle_params_.push_back(std::move(vehicle_params));
 
-    std::shared_ptr<MultiRotorConnector> vehicle = std::make_shared<MultiRotorConnector>(
-        wrapper, vehicle_params_.back().get(), enable_rpc, api_server_address,
-        vehicle_params_.back()->getParams().api_server_port, manual_pose_controller);
+	std::shared_ptr<MultiRotorConnector> vehicle = std::make_shared<MultiRotorConnector>(
+		wrapper, vehicle_params_.back().get(), enable_rpc, api_server_address,
+		vehicle_params_.back()->getParams().api_server_port, manual_pose_controller);
 
-    if (vehicle->getPhysicsBody() != nullptr)
-        wrapper->setKinematics(&(static_cast<PhysicsBody*>(vehicle->getPhysicsBody())->getKinematics()));
+	if (vehicle->getPhysicsBody() != nullptr)
+		wrapper->setKinematics(&(static_cast<PhysicsBody*>(vehicle->getPhysicsBody())->getKinematics()));
 
-    return std::static_pointer_cast<VehicleConnectorBase>(vehicle);
+	return std::static_pointer_cast<VehicleConnectorBase>(vehicle);
 }
 
 
