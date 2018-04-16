ef85b3d41827760a641234dcd64c4186d88e63ac
Hasan Genc <hngenc@utexas.edu>
2018-04-16 12:49:58 -0500

    Added CommonPython files that bring all Python initialization and finalization routines into one cpp file

---
+++ AirLib/include/common/CommonPython.hpp
@@ -0,0 +1,18 @@
+#ifndef COMMON_PYTHON_HPP
+#define COMMON_PYTHON_HPP
+
+#include <Python.h>
+#include <string>
+
+extern PyObject * pModule;
+
+void initializePython();
+int finalizePython();
+
+PyObject * getNoiseFunc(std::string noise_name);
+PyObject * getDepthNoiseFunc();
+
+void python_lock();
+void python_unlock();
+
+#endif
---
+++ AirLib/src/common/CommonPython.cpp
@@ -0,0 +1,98 @@
+#include "common/CommonPython.hpp"
+#include <Python.h>
+#include <string>
+#include <mutex>
+
+static const std::string module_name = "sensor_noise";
+PyObject * pModule = nullptr;
+
+static PyObject * pDepthNoiseFunc = nullptr;
+static bool depthNoiseFuncInitialized = false;
+
+// Globals to lock and unlock python
+static std::mutex python_mutex;
+
+// Globals to keep track of when to finalize and initialize
+static const int sensors_num = 3;
+static int sensors_exited = 0;
+
+void initializePython() {
+	python_lock();
+
+	Py_Initialize();
+	sensors_exited = 0;
+
+	if (pModule == nullptr) {
+		PyObject *pName = PyUnicode_FromString(module_name.c_str());
+		pModule = PyImport_Import(pName);
+		Py_DECREF(pName);
+
+		if (pModule == nullptr) {
+			throw "python module could not be imported";
+		}
+	}
+
+	python_unlock();
+}
+
+int finalizePython() {
+	python_lock();
+
+	sensors_exited++;
+
+	if (sensors_exited == sensors_num) {
+		Py_DECREF(pModule);
+		Py_XDECREF(pDepthNoiseFunc);
+		Py_Finalize();
+
+		pModule = nullptr;
+		pDepthNoiseFunc = nullptr;
+		depthNoiseFuncInitialized = false;
+	}
+
+	python_unlock();
+
+	return sensors_num - sensors_exited;
+}
+
+PyObject * getNoiseFunc(std::string noise_name) {
+	python_lock();
+
+	PyObject * pNoiseFunc;
+
+	if (pModule != NULL) {
+		pNoiseFunc = PyObject_GetAttrString(pModule, noise_name.c_str());
+		if (!(pNoiseFunc && PyCallable_Check(pNoiseFunc))) {
+			//can't do it
+			pNoiseFunc = nullptr;
+			PyErr_Print();
+		}
+	}
+	else {
+		//can't do it
+		pNoiseFunc = nullptr;
+		PyErr_Print();
+	}
+
+	python_unlock();
+	return pNoiseFunc;
+}
+
+PyObject * getDepthNoiseFunc() {
+	if (!depthNoiseFuncInitialized) {
+		pDepthNoiseFunc = getNoiseFunc("depth_noise");
+		depthNoiseFuncInitialized = true;
+	}
+
+	return pDepthNoiseFunc;
+}
+
+void python_lock()
+{
+	python_mutex.lock();
+}
+
+void python_unlock()
+{
+	python_mutex.unlock();
+}
