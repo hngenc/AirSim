c6006a9eb5fbe9fb1605fd08a167a69b5cfcd172
Hasan Genc <hngenc@utexas.edu>
2018-05-28 21:33:42 -0500

    Added Matrice 100 and 3DR Solo models

--- AirLib/include/vehicles/multirotor/configs/SimpleFlightQuadX.hpp
+++ AirLib/include/vehicles/multirotor/configs/SimpleFlightQuadX.hpp
@@ -9,6 +9,11 @@
 #include "common/AirSimSettings.hpp"
 #include "sensors/SensorFactory.hpp"
 
+// Setup the drone's parameters based on several pre-defined models
+// #define F450
+// #define _3DR_SOLO
+#define MATRICE_100
+
 
 namespace msr { namespace airlib {
 
@@ -27,24 +32,57 @@ protected:
         auto& params = getParams();
 
         /******* Below is same config as PX4 generic model ********/
+#ifdef F450
+        /* TODO: calculate and un-comment max_rpm and propeller_diameter */
+        params.rotor_count = 4;
+        params.mass = 1.0f;
+        real_T motor_assembly_weight = 0.055f;
+        real_T motor_arm_length = 0.2275f;
+        params.rotor_params.C_P = 0.040164f; // the torque co-efficient
+        params.rotor_params.C_T = 0.109919f; // the thrust co-efficient
+        // params.rotor_params.max_rpm = 13024 * .6; // RPM can be obtained by KV * Voltage
+        // params.rotor_params.propeller_diameter = 0.254f; // in meters
+
+        params.body_box.x() = 0.180f; params.body_box.y() = 0.11f; params.body_box.z() = 0.040f;
+        real_T rotor_z = 2.5f / 100;
+#elif defined(_3DR_SOLO)
+        /* TODO: make a new rotor_params and update it with the correct coefficents, rpm, and prop diameter */
+        params.rotor_count = 4;
+        params.mass = 1.5f;
+        real_T motor_assembly_weight = 0.0680389f;
+        real_T motor_arm_length = .130175f;
+        params.rotor_params.C_P = 0.040164f; // the torque co-efficient
+        params.rotor_params.C_T = 0.109919f; // the thrust co-efficient
+        params.rotor_params.max_rpm = 13024 * .6; //RPM can be obtained by KV * Voltage
+        params.rotor_params.propeller_diameter = 0.254f; //in meters
+
+        params.body_box.x() = .2428875f; params.body_box.y() = .10795; params.body_box.z() = .066675;
+        real_T rotor_z = 0.9525f / 100;
+#elif defined(MATRICE_100)
+        /* TODO: recalculate thrust coefficent to account for propeller efficency */
+        params.rotor_count = 4;
+        params.mass = 2.431f;
+        real_T motor_assembly_weight = 0.106f;
+        real_T motor_arm_length = .325f;
+        params.rotor_params.C_P = .033526f; // the torque co-efficient
+        params.rotor_params.C_T = .027122f; // the thrust co-efficient
+        params.rotor_params.max_rpm = 8750; // RPM can be obtained by KV * Voltage (or from docs)
+        params.rotor_params.propeller_diameter = 0.3302f; //in meters
+
+        params.body_box.x() = .252f; params.body_box.y() = .190f; params.body_box.z() = .131f;
+        real_T rotor_z = .0569f;
+#endif
 
         //set up arm lengths
         //dimensions are for F450 frame: http://artofcircuits.com/product/quadcopter-frame-hj450-with-power-distribution
-        params.rotor_count = 4;
-        std::vector<real_T> arm_lengths(params.rotor_count, 0.2275f);
+        std::vector<real_T> arm_lengths(params.rotor_count, motor_arm_length);
 
         //set up mass
-        params.mass = 1.0f; //can be varied from 0.800 to 1.600
-        real_T motor_assembly_weight = 0.055f;  //weight for MT2212 motor for F450 frame
         real_T box_mass = params.mass - params.rotor_count * motor_assembly_weight;
 
         // using rotor_param default, but if you want to change any of the rotor_params, call calculateMaxThrust() to recompute the max_thrust
         // given new thrust coefficients, motor max_rpm and propeller diameter.
         params.rotor_params.calculateMaxThrust();
-
-        //set up dimensions of core body box or abdomen (not including arms).
-        params.body_box.x() = 0.180f; params.body_box.y() = 0.11f; params.body_box.z() = 0.040f;
-        real_T rotor_z = 2.5f / 100;
 
         //computer rotor poses
         initializeRotorQuadX(params.rotor_poses, params.rotor_count, arm_lengths.data(), rotor_z);
